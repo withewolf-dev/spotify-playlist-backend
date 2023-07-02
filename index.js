59f95a15d791eb01fd1ed9373ea94aa85428611f
stn91-git <111347309+stn91-git@users.noreply.github.com>
2023-07-02 18:20:42 +0530

    puppeter working

--- index.js
+++ index.js
@@ -195,8 +195,8 @@ async function searchSongsOnYouTube(songs) {
   return playlistLink;
 }
 
-// app.listen(4000, () => {
-//   console.log("Server is running on port 4000");
-// });
+app.listen(4000, () => {
+  console.log("Server is running on port 4000");
+});
 
 exports.api = functions.https.onRequest(app);
